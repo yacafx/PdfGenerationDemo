32b10f97726eb308cc064122616151dd966fbedc
yacaFx <sergiobritor@gmail.com>
2013-12-04 14:54:24 -0600

    Set all copies and sample graph images

--- PdfGenerationDemo/PdfGenerationDemoViewController.m
+++ PdfGenerationDemo/PdfGenerationDemoViewController.m
@@ -18,7 +18,7 @@
 
 #pragma mark - Private Methods
 
-NSString *text1 = @"<nombre> a continuación te presentamos información importante para construir el patrimonio que te permitirá vivir tu retiro en plenitud, estos datos están acorde con tu situación de vida actual y los datos que nos has proporcionado.  \r\r Con tu ahorro programado del <%% ahorro> %% de tu ingreso mensual, al llegar a los <edad> años tendrás un monto de $<cantidad ahorrada> que te permitirá vivir el retiro que siempre imaginaste.";
+NSString *text1 = @"<nombre> a continuación te presentamos información importante para construir el patrimonio que te permitirá vivir tu retiro en plenitud, estos datos están acorde con tu situación de vida actual y los datos que nos has proporcionado.  \r\r Con tu ahorro programado del <% ahorro> % de tu ingreso mensual, al llegar a los <edad> años tendrás un monto de $<cantidad ahorrada> que te permitirá vivir el retiro que siempre imaginaste.";
 
 NSString *text2 = @"Con este ahorro para tu retiro podrás mantener tu nivel de vida y el de tu familia al llegar a tu retiro, por lo que es importante que conozcas cómo están conformados los ingresos que recibirás al momento de retirarte.";
 
@@ -27,26 +27,25 @@ NSString *text3 = @"También es importante saber en qué estarás gastando por l
 NSString *text4 = @"Para nosotros es importante acompañarte en la planeación de cada etapa de tu vida y contamos con soluciones acordes a tus necesidades, por lo que te invitamos a acercarte con alguno de nuestros asesores, quien podrá hacerte un traje a la medida de tus necesidades.";
 
 
-- (void) drawText:(NSString *)imageName
+- (void) drawText:(NSString *)textToShow posY:(float)posY
 {
     CGContextRef currentContext = UIGraphicsGetCurrentContext();
     CGContextSetRGBFillColor(currentContext, 0.0, 0.0, 0.0, 1.0);
     
-    NSString *textToDraw = imageName;
+    NSString *textToDraw = textToShow;
     
-    UIFont *font = [UIFont systemFontOfSize:14.0];
+    UIFont *font = [UIFont fontWithName:@"Helvetica" size:16.0];
     
     CGSize stringSize = [textToDraw sizeWithFont:font
                                constrainedToSize:CGSizeMake(pageSize.width - 2*kBorderInset-2*kMarginInset, pageSize.height - 2*kBorderInset - 2*kMarginInset)
                                    lineBreakMode:UILineBreakModeWordWrap];
     
-    CGRect renderingRect = CGRectMake(kBorderInset + kMarginInset, 280, pageSize.width - 2*kBorderInset - 2*kMarginInset, stringSize.height);
+    CGRect renderingRect = CGRectMake(kBorderInset + kMarginInset, posY, pageSize.width - 2*kBorderInset - 2*kMarginInset, stringSize.height);
     
     [textToDraw drawInRect:renderingRect
                   withFont:font
              lineBreakMode:UILineBreakModeWordWrap
                  alignment:UITextAlignmentLeft];
-    
 }
 
 - (void) drawImage:(NSString *)imageName posX:(float)posX posY:(float)posY
@@ -70,11 +69,32 @@ NSString *text4 = @"Para nosotros es importante acompañarte en la planeación d
         //Draw a page number at the bottom of each page.
         currentPage++;
         
-        //Draw some text for the page.
-        [self drawText:text1];
-        
-        //Draw an image
+        //Draw header image
         [self drawImage:@"headerPdf.png" posX:0 posY:0];
+        
+        //Draw footer image
+        [self drawImage:@"footerPdf.png" posX:0 posY:1436];
+        
+        //Draw graphic 1
+        [self drawImage:@"graph1.png" posX:211.5 posY:390];
+        
+        //Draw graphic 2
+        [self drawImage:@"graph2.png" posX:211.5 posY:730];
+        
+        //Draw graphic 3
+        [self drawImage:@"graph3.png" posX:211.5 posY:1070];
+        
+        //Draw paragraph 1
+        [self drawText:text1 posY:280];
+        
+        //Draw paragraph 2
+        [self drawText:text2 posY:670];
+        
+        //Draw paragraph 3
+        [self drawText:text3 posY:1015];
+        
+        //Draw paragraph 4.
+        [self drawText:text4 posY:1350];
         
         
         done = YES;
@@ -109,7 +129,7 @@ NSString *text4 = @"Para nosotros es importante acompañarte en la planeación d
 
 - (IBAction)generatePdfButtonPressed:(id)sender
 {
-    pageSize = CGSizeMake(1024, 1008);
+    pageSize = CGSizeMake(1024, 1450);
     NSString *fileName = @"ViveTuRetiro.pdf";
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
